78fe60ff6dadc18df1032ce3d89028f419ebc881
William Luke <william@ordino.ai>
2021-03-10 17:59:51 +0300

    init

---
+++ main.test.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+test("query logs", async () => {
+  const datamodel = fs
+    .readFileSync(path.join(__dirname, "schema.prisma"))
+    .toString();
+  process.env.BLOOP = "file:dev.db";
+  const engine = require(`./libquery_engine_napi.so.node`);
+
+  let qe = new engine.QueryEngine(
+    {
+      datamodel: datamodel,
+      logLevel: "info",
+      datasourceOverrides: {},
+    },
+    (err, log) => {
+      console.log(err, log);
+    }
+  );
+});
+
